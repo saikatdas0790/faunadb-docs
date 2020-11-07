09b8c8033ef3992fcb27e1322f948b7ce3bd8418
Saikat Das <saikatdas0790@gmail.com>
2020-11-07 16:33:04 +0530

    reinitialize projects again

--- src/routes/cookbook.svelte
+++ src/routes/cookbook.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import LogArea from "../components/LogArea.svelte";
+  import { someObject } from "../keys";
+  console.log(someObject);
   let logDetails: object;
 </script>
 
